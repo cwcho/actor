3e9049646136c35a6758a6beee71e2d133892cd1
dongwon <eastbest1@gmail.com>
2013-12-15 05:56:12 +0900

    Sun Dec 15 05:56:12 KST 2013

--- ymb/models.py
+++ ymb/models.py
@@ -1,31 +1,28 @@
 from django.db import models
 
-class Customer(models,Model):
-    pwd         = models.CharField(max_langth=100)
-    name        = models.CharField(max_langth=60)
-    email       = models.EmailField(max_langth=75,unique)
-    phone       = models.CharField(max_langth=30)
-    address     = models.CharField(max_langth=500)
+from django.db import models
 
-class Shop(models,Model):
-    name        = models.CharField(max_langth=50)
-    phone       = models.CharField(max_langth=30)
-    address     = models.CharField(max_langth=500)
-
-class Menu(models,Model):
-    name        = models.CharField(max_langth=100)
+class Customer(models.Model):
+    pwd         = models.CharField(max_length=100)
+    name        = models.CharField(max_length=60)
+    email       = models.EmailField(max_length=75, unique=True)
+    phone       = models.CharField(max_length=30)
+    address     = models.CharField(max_length=500)
+class Shop(models.Model):
+    name        = models.CharField(max_length=50)
+    phone       = models.CharField(max_length=30)
+    address     = models.CharField(max_length=500)
+class Menu(models.Model):
+    name        = models.CharField(max_length=100)
     price       = models.PositiveIntegerField()
-
-class Order(models,Model):
+class Order(models.Model):
     customer    = models.ForeignKey(Customer)
     shop        = models.ForeignKey(Shop)
     bill        = models.PositiveIntegerField()
     date        = models.DateField()
-
-class Order_specific(models,Model):
+class Order_specific(models.Model):
     order       = models.ForeignKey(Order)
     menu        = models.ForeignKey(Menu)
     qty         = models.PositiveSmallIntegerField()
 
 
-# Create your models here.
