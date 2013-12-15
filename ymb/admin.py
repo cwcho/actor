6f6141277fc06b8507218d4bec7ab8f4f4b78424
dongwon <eastbest1@gmail.com>
2013-12-15 18:30:14 +0900

    Sun Dec 15 18:30:14 KST 2013

--- ymb/admin.py
+++ ymb/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from ymb.models import Shop, Menu
+from ymb.models import Shop, Menu, Customer
 
 admin.site.register(Shop)
 admin.site.register(Menu)
+admin.site.register(Customer)
 # Register your models here.
--- ymb/views.py
+++ ymb/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from ymb.models import Customer
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the poll index.")
+    latest_customer_list = Customer.objects.order_by('-id')[:5]
+    context = {'latest_customer_list': latest_customer_list}
+    return render(request, 'ymb/index.html', context)
+
 
 # Create your views here.
