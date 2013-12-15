abbb70dfe25acd09e4a4017a424eed373cd7ec70
dongwon <eastbest1@gmail.com>
2013-12-15 21:36:10 +0900

    Sun Dec 15 21:36:10 KST 2013

--- ymb/urls.py
+++ ymb/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^sign_up$', views.sign_up, name='sign_up'),
     url(r'^proc_sign_up$', views.proc_sign_up, name='proc_sign_up'),
+    url(r'^menu$', views.menu, name='menu'),
 )
--- ymb/views.py
+++ ymb/views.py
@@ -24,5 +24,9 @@ def proc_sign_up(request):
     print request.POST['sex']
     return HttpResponseRedirect("/")
 
+def menu(request):
+    latest_customer_list = Customer.objects.order_by('-id')[:5]
+    context = {'latest_customer_list': latest_customer_list}
+    return render(request, 'ymb/menu.html', context)
 #def sign_up_result(request):
 #    
