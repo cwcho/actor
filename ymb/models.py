eda24249b53b2ad5513b1d5ef42e50385ff4c6c5
dongwon <eastbest1@gmail.com>
2013-12-15 22:25:34 +0900

    Sun Dec 15 22:25:34 KST 2013

--- ymb/models.py
+++ ymb/models.py
@@ -8,21 +8,34 @@ class Customer(models.Model):
     email       = models.EmailField(max_length=75, unique=True)
     phone       = models.CharField(max_length=30)
     address     = models.CharField(max_length=500)
+    def __unicode__(self):
+        return self.name
+
 class Shop(models.Model):
     name        = models.CharField(max_length=50)
     phone       = models.CharField(max_length=30)
     address     = models.CharField(max_length=500)
+    def __unicode__(self):
+        return self.name
+
 class Menu(models.Model):
     name        = models.CharField(max_length=100)
     price       = models.PositiveIntegerField()
+    def __unicode__(self):
+        return self.name
+
 class Order(models.Model):
     customer    = models.ForeignKey(Customer)
     shop        = models.ForeignKey(Shop)
     bill        = models.PositiveIntegerField()
     date        = models.DateField()
+    def __unicode__(self):
+        return self.id
+
 class Order_specific(models.Model):
     order       = models.ForeignKey(Order)
     menu        = models.ForeignKey(Menu)
     qty         = models.PositiveSmallIntegerField()
-
+    def __unicode__(self):
+        return self.id
 
