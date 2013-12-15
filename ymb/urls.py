33fd6422187f971cc641572f27fc7c1656077734
dongwon <eastbest1@gmail.com>
2013-12-15 19:14:34 +0900

    Sun Dec 15 19:14:34 KST 2013

--- ymb/urls.py
+++ ymb/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import patterns, url
 from ymb import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^sign_up$', views.sign_up, name='sign_up'),
+    url(r'^proc_sign_up$', views.proc_sign_up, name='proc_sign_up'),
 )
--- ymb/views.py
+++ ymb/views.py
@@ -9,5 +9,10 @@ def index(request):
     context = {'latest_customer_list': latest_customer_list}
     return render(request, 'ymb/index.html', context)
 
+def sign_up(request):
+    latest_customer_list = Customer.objects.order_by('-id')[:5]
+    context = {'latest_customer_list': latest_customer_list}
+    return render(request, 'ymb/sign_up.html', context)
 
-# Create your views here.
+def proc_sign_up(request):
+    return HttpResponse(request.POST['name'])
