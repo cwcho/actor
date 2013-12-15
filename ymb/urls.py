adbb629d9b258fa2723b6f8e2f6117977537e9b9
dongwon <eastbest1@gmail.com>
2013-12-16 03:32:03 +0900

    Mon Dec 16 03:32:03 KST 2013

--- ymb/urls.py
+++ ymb/urls.py
@@ -4,9 +4,11 @@ from ymb import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^sign_in$', views.sign_in, name='sign_in'),
+    url(r'^sign_in_fail$', views.sign_in_fail, name='sign_in_fail'),
     url(r'^sign_up$', views.sign_up, name='sign_up'),
     url(r'^proc_sign_up$', views.proc_sign_up, name='proc_sign_up'),
-    url(r'^menu$', views.menu, name='menu'),
+    url(r'^sign_up_result$', views.sign_up_result, name='sign_up_result'),
     url(r'^check_email$', views.check_email, name='check_email'),
     url(r'^check_address$', views.check_address, name='check_address'),
     url(r'^order$', views.order, name='order'),
--- ymb/views.py
+++ ymb/views.py
@@ -2,32 +2,46 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext, loader
 from django.views.decorators.csrf import csrf_exempt
+from django.db.models import Q
 
 from ymb.models import Customer
 
 def index(request):
-    latest_customer_list = Customer.objects.order_by('-id')[:5]
-    context = {'latest_customer_list': latest_customer_list}
-    return render(request, 'ymb/index.html', context)
+    return render(request, 'ymb/index.html')
+
+@csrf_exempt
+def sign_in(request):
+    from ymb.models import Customer
+    print request.POST['email']
+    print request.POST['password']
+    from ymb.models import Customer
+    try:
+        Customer.objects.get(Q(email__exact=request.POST['email']) & Q(pwd__exact=request.POST['password']))
+    except Customer.DoesNotExist:
+        return HttpResponseRedirect("/sign_in_fail")
+    else:
+        return HttpResponseRedirect("/order")
 
 def sign_up(request):
-    latest_customer_list = Customer.objects.order_by('-id')[:5]
-    context = {'latest_customer_list': latest_customer_list}
-    return render(request, 'ymb/sign_up.html', context)
+    return render(request, 'ymb/sign_up.html')
 
 @csrf_exempt
 def proc_sign_up(request):
+    from ymb.models import Customer
     print request.POST['name']
     print request.POST['password']
     print request.POST['email']
+    print request.POST['phone']
     print request.POST['address']
-    print request.POST['sex']
-    return HttpResponseRedirect("/")
+    c = Customer(name = request.POST['name'], pwd = request.POST['password'], email = request.POST['email'], phone = request.POST['phone'], address = request.POST['address'])
+    print "c"
+    c.save()
+    return HttpResponseRedirect("/sign_up_result")
 
-def menu(request):
+def sign_up_result(request):
     latest_customer_list = Customer.objects.order_by('-id')[:5]
     context = {'latest_customer_list': latest_customer_list}
-    return render(request, 'ymb/menu.html', context)
+    return render(request, 'ymb//sign_up_result.html', context)
 
 def check_email(request):
     from ymb.models import Customer
@@ -50,10 +64,7 @@ def check_address(request):
         return HttpResponse("true")
 
 def order(request):
-    latest_customer_list = Customer.objects.order_by('-id')[:5]
-    context = {'latest_customer_list': latest_customer_list}
-    return render(request, 'ymb/order.html', context)
+    return render(request, 'ymb/order.html')
 
-
-#def sign_up_result(request):
-#    
+def sign_in_fail(request):
+    return render(request, 'ymb/sign_in_fail.html')
